a54df411515eb265b8ec09630db3a185782a2720
ihsankarim <ihsanalkarim305@gmail.com>
2025-06-26 09:49:08 +0700

    initialize project with config database

---
+++ config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDatabase() {
+	dsn := fmt.Sprintf("host=%s passwod=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect database: " + err.Error())
+	}
+
+	DB = db
+}
